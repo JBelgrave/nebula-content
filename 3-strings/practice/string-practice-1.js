8216adf5c8ae4fdc9df458b6ee70e5b9c83188ed
JBelgrave <JudahBelgrave@gmail.com>
2023-03-08 15:49:46 -0500

    added string practice-1

---
+++ 3-strings/practice/string-practice-1.js
@@ -0,0 +1,33 @@
+/* 
+Copy the following text to your notes, then create variables and assign them strings that will help the bottom console.log run properly:
+
+Write your code below the comment and above the console log 
+
+
+const language =    `Javascript`
+const adjective1 = `love`
+const keyword1 = `var`
+const year = 2015
+const instructor = `Mr. Eric`
+
+
+console.log(`I am currently learning the programming language called ${language}. I ${adjective1} this program already. I read recently that declaring variables with the ${keyword1} keyword has gone out of style ever since ECMAScript ${year}. Oh, also, my instructor ${instructor} is awesome!`);
+*/
+
+/*let city = 'Manhattan'
+console.log(city.length)
+
+let lastChar = city[city.length-1]
+console.log(lastChar)*/
+
+
+// Using only the alphabet variable below, bracket notation & concatenation, console.log out your first name
+
+const alph = "abcdefghijklmnopqrstuvwxyz";
+let my1Letter = alph[9]
+let my2Letter = alph[20]
+let my3Letter = alph[3]
+let my4Letter = alph[0]
+let my5Letter = alph[7]
+
+console.log(my1Letter + my2Letter + my3Letter + my4Letter + my5Letter);
