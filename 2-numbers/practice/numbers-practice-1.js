b6f370bfefc9ade18216d72deeb203643943900b
JBelgrave <JudahBelgrave@gmail.com>
2023-03-08 13:35:04 -0500

    added numbers-practice-1 and numbers-practice-2

---
+++ 2-numbers/practice/numbers-practice-1.js
@@ -0,0 +1,36 @@
+// Declare a variable and assign it to a numeric value
+// Double that value and re-assign it to the previous variable
+
+
+
+// Create two numbers, assign them to variables and add them together
+ let a = 2
+ let b = 4
+ a += b
+ console.log(a)
+
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+ let c = 3
+ let d = 7
+ let e = 5 
+ d -= c + e
+ console.log(d)
+
+// Create two even numbers, assign them to variables and divide one from the other
+ let f = 6
+ let g = 12
+ g /= f
+ console.log(g)
+
+// Create two odd numbers, assign them to variables and multiply one from the other
+ let h = 3
+ let i = 5
+ h *= i
+ console.log(h)
+
+// Create a variable and log it's remainder, if divided by 3, to the console 
+let j = 9
+console.log(j % 3)
+
+// Answer in plain text, what is modulo especially useful for?
+// Determining wether a given number is odd or even
