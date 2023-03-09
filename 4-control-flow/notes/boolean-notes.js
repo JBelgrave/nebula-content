f4e4af750740b79e54b2f9ca267814270dcb7a55
JBelgrave <JudahBelgrave@gmail.com>
2023-03-09 15:48:23 -0500

    added boolean-notes

---
+++ 4-control-flow/notes/boolean-notes.js
@@ -0,0 +1,70 @@
+/*// This is an expression which evaluates to the value true
+//console.log(5>2)
+
+// This is an expression which evaluates to the value false
+//console.log(6 === "6")
+
+//const storedEvaluation = 10 < 5
+//console.log(storedEvaluation)
+
+
+// Evaluates to the boolean true
+console.log("3" > "2")
+
+// Evaluates to the boolean false
+console.log(5 > "10")
+
+// Evaluates to the boolean true - because 20 is greater than or equal to 20
+console.log(20 >= 20)
+
+// Evaluates to true
+console.log("a" < "b")
+
+//Evaluates to true
+console.log("e" > "d")
+
+// Evaluates to false
+console.log("a" > "z")
+
+if(20 > 10){
+console.log("THIS IS TRUE!!!")
+}
+
+let agent = "James Bond";
+if(agent === "James Bond"){
+  console.log("Welcome, Bond. Lets go over your mission debrief.");
+}
+
+
+let agent = "James Bond";
+if(agent === "James Bond"){
+  console.log("Welcome, Bond. Lets go over your mission debrief.");
+} else {
+  console.log("Intruder Alert! You have 5 seconds to leave the premises or be dealt with!");
+}
+
+
+
+let agent = "Johnny Bravo";
+
+if(agent === "James Bond"){
+  console.log("Welcome, Bond. Lets go over your mission debrief.");}
+
+else if(agent === "Johnny Bravo"){
+  console.log("You again?! Get out of here Johnny, before I ring the alarm.");}
+
+else {console.log("Intruder Alert! You have 5 seconds to leave the premises or be dealt with!");}
+
+
+if("this string has a truthy value"){
+    console.log("If you see this message, the condition was truthy");}
+
+  if(0){
+    console.log("You'll never see this message with a zero as a condition. The number 0 is a falsy value.");}
+
+  if(null){
+    console.log("This won't work either");}
+
+  if(1){
+    console.log("You'll see this though!");}
+*/
