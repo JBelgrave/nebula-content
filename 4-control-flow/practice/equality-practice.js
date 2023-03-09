3f077bb724e15a3f3bc6cb93dc0a326cb0a6f604
JBelgrave <JudahBelgrave@gmail.com>
2023-03-09 15:47:36 -0500

    added comparison, equality, and horse-race practices

---
+++ 4-control-flow/practice/equality-practice.js
@@ -0,0 +1,35 @@
+// Create two variables, one equal to '10' and the other equal to 10
+const a = '10'
+const b = 10
+
+// Comparing the two variables, are they strictly equal?
+console.log(a === b)
+
+// Comparing the two variables, are they loosely equal?
+console.log(a == b)
+
+// Comparing our two prior variables, in one statement, are they both loosely equal and strictly equal? (Using the && operator)
+
+
+// Comparing our two prior variables, in one statement, are they either loosely equal or strictly equal? (Using the || operator)
+
+
+// Check if the variable set to 10 is not loosely equal to 'ten' 
+console.log(b != 'ten')
+
+// Create a variable and initilize it to true, check if it is not strictly equal to 'true' 
+const bool = true
+console.log(bool !== 'true')
+
+
+/*console.log(null === undefined) - false
+
+console.log(5 == "5") - true
+
+console.log(10 !== "10") - true
+
+console.log(20 === "20") - false
+
+console.log(20 >= "20") - true
+
+console.log(30 < 35) - true*/
