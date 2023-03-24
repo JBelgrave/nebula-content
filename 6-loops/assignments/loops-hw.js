8d381440bba3507b2c55e57580f17fc144896cea
JBelgrave <JudahBelgrave@gmail.com>
2023-03-24 01:55:16 -0400

    updated about-me

--- 6-loops/assignments/loops-hw.js
+++ 6-loops/assignments/loops-hw.js
@@ -1,14 +1,14 @@
 // 1.   Declare a for loop that starts at 0, goes until 30, and increments by 1
 //      Each loop it should log to the console 'the current index is _'
 for(a = 0; a <= 30; a++){
-    console.log(`The current index is ${a}`);
+    //console.log(`The current index is ${a}`);
 }
 
 
 // 2.   Declare a for loop that starts at 1, goes until 10, and increments by 2
 //       Each loop it should log to the console the current index
 for(b = 1; b < 10; b += 2){
-    console.log(`The current index is ${b}`);
+    //console.log(`The current index is ${b}`);
 }
 
 
@@ -17,7 +17,7 @@ for(b = 1; b < 10; b += 2){
 //      Loop over the array and log each celebrity's full name to the console.
 let celeb = ['Jennifer Lopez', 'Johnny Depp', 'Tom Cruise', 'Adam Sandler', 'Betty White'];
 for(c = 0; c < celeb.length; c++){
-    console.log(celeb[c]);
+    //console.log(celeb[c]);
 }
 
 
@@ -25,7 +25,7 @@ for(c = 0; c < celeb.length; c++){
 //      If a celebrity's full name has an even number of characters log it to the console
 for(d = 0; d < celeb.length; d++){
     if((d - 1) % 2 === 0){
-        console.log(celeb[d]);
+        //console.log(celeb[d]);
     }
 }
 
@@ -37,7 +37,7 @@ for(e = 0; e < celeb.length; e++){
     let celebFirst = celeb[e].split(' ');
     celebFirstArray.push(celebFirst[0]);
 }
-console.log(celebFirstArray);
+//console.log(celebFirstArray);
 
 
 // 6.   Use the celebrity array for this question
@@ -47,12 +47,12 @@ for(f = 0; f < celeb.length; f++){
     let celebLast = celeb[f].split(' ');
     celebLastArray.push(celebLast[1]);
 }
-console.log(celebLastArray);
+//console.log(celebLastArray);
 
 
 // 7.   Loop over the celebrityist and then loop through each name. 
 //      If a letter is a vowel, log it to the console
-const vowels = 'aeiou'
+const vowels = 'aeiouAEIOU'
 for(g = 0; g < celeb.length; g++){
     const name = celeb[g]
     for(h = 0; h < name.length; h++){
@@ -72,7 +72,7 @@ for(i = 0; i < celeb.length; i++){
     for(j = 0; j < name.length; j++){
         const char = name[j]
         if(upper.includes(char)){
-            console.log(char)
+            //console.log(char)
         }
     }
 }
@@ -84,7 +84,7 @@ for(i = 0; i < celeb.length; i++){
 //        Input:  ['Martha Stewart', 'David Beckham', etc..] 
 //        Output: ['MARTHA-STEWART', 'DAVID-BECKHAM', etc..]
 const celebUpper = celeb.toString().toUpperCase().split(" ").join("-").split(",")
-console.log(celebUpper)
+//console.log(celebUpper)
 
 // 10.  Index your array to find your favorite celebrity. 
 //      Save that name to a variable
@@ -93,7 +93,7 @@ console.log(celebUpper)
 const fav = celeb[3]
 for(k = 0; k < celeb.length; k++){
     if(fav === celeb[k]){
-        console.log(celeb[k])
+        //console.log(celeb[k])
         break;
     }
 
@@ -109,12 +109,12 @@ for(k = 0; k < celeb.length; k++){
 //      Otherwise print the index to the console
 for(l = 0; l <= 30; l++){
     if(l % 2 === 0 && l % 3 !== 0){
-        console.log(l, "fizz")
+        //console.log(l, "fizz")
     }else if(l % 2 !== 0 && l % 3 === 0){
-        console.log(l, "buzz")
+        //console.log(l, "buzz")
     }else if (l % 2 === 0 && l % 3 === 0){
-        console.log(l, "fizz buzz")
+        //console.log(l, "fizz buzz")
     }else{
-        console.log(l)
+        //console.log(l)
     }
 }
