35cddb9ddcd1d419b6803800ae32e8e04b2b4627
JBelgrave <JudahBelgrave@gmail.com>
2023-03-30 00:53:17 -0400

    completed dom-manipulation-hw

---
+++ 11-dom-manipulation/practice/dom-practice.html/script.js
@@ -0,0 +1,21 @@
+// Grab the elements and log them to the console
+
+// Q1:  Attach this document to the index.html
+
+
+// Q2:  Grab the second element that has the text Hello
+const hello2 = document.querySelector("#first");
+console.log(hello2)
+
+// Q3:  Grab the text in the h1 that says 'Good Afternoon'
+const h1s = document.querySelectorAll("h1");
+console.log(h1s[2])
+
+
+// Q4:  Grab the h1 with the text that says "Catch me if you can"
+const catchMe = document.querySelectorAll("Catch me if you can");
+console.log(catchMe)
+
+// Q5:  Grab the span element and change the text to "Got ya"
+const gitYa = document.querySelector("span");
+console.log(gitYa)
